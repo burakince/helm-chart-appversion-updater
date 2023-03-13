30ef9c079b41ee57135b7c347259b9f846d6edef
Burak Ince <burak.ince@linux.org.tr>
2023-03-14 00:07:51 +0100

    Add more debug sections
    Signed-off-by: Burak Ince <burak.ince@linux.org.tr>

--- run.sh
+++ run.sh
@@ -24,7 +24,12 @@ URL="${PLUGIN_SSH_URL:?SSH URL empty or unset}"
 if compgen -G "~/.ssh/id_rsa" > /dev/null; then
   SSH_KEY="${PLUGIN_SSH_KEY:?SSH Key empty or unset}"
   echo -n "$SSH_KEY" > ~/.ssh/id_rsa
-  chmod 600 /root/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+
+  if [ "$DEBUG_MODE" = true ] ; then
+    cat  ~/.ssh/id_rsa
+    ls -al  ~/.ssh/
+  fi
 fi
 
 HOSTNAME=""
@@ -58,6 +63,11 @@ else
 fi
 
 ssh-keyscan -t rsa $HOSTNAME >> ~/.ssh/known_hosts
+
+if [ "$DEBUG_MODE" = true ] ; then
+  cat ~/.ssh/known_hosts
+fi
+
 EMAIL="${PLUGIN_EMAIL:?User email empty or unset}"
 git config --global user.email "$EMAIL"
 NAME="${PLUGIN_NAME:?User name empty or unset}"
@@ -67,6 +77,11 @@ IMAGE_TAG="${PLUGIN_IMAGE_TAG:?Image tag empty or unset}"
 rm -rf $REPOSITORY
 git clone git@$HOSTNAME:$USER/$REPOSITORY.git
 cd $REPOSITORY
+
+if [ "$DEBUG_MODE" = true ] ; then
+  ls -al
+fi
+
 CURRENT_BRANCH=$(git branch --show-current)
 
 CHART_PATH="${PLUGIN_CHART_PATH:?Chart folder path empty or unset}"
@@ -82,6 +97,10 @@ CHART_VERSION_NEXT="${CHART_VERSION%.*}.$((${CHART_VERSION##*.}+1))"
 
 sed -i'.bak' -e 's|^version:.*|version: '"$CHART_VERSION_NEXT"'|g' $CHART_PATH/Chart.yaml
 sed -i'.bak' -e 's|^appVersion:.*|appVersion: '"$IMAGE_TAG"'|g' $CHART_PATH/Chart.yaml
+
+if [ "$DEBUG_MODE" = true ] ; then
+  cat $CHART_PATH/Chart.yaml
+fi
 
 echo "Commiting chart source changes to $CURRENT_BRANCH branch"
 git add $CHART_PATH/Chart.yaml
